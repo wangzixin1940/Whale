d7410ee771f38d3c525797341ca75d2d88730fa9
Zixin Wang <wangzixin1940@outlook.com>
2025-03-16 17:45:46 +0800

    Add a setting function (the specific function has not yet been implemented)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QApplication, QFileDialog, QMessageBox
+from PySide6.QtWidgets import QApplication, QFileDialog, QMessageBox, QInputDialog
 from PySide6.QtGui import QIcon
 from PySide6.QtCore import QTranslator
 from PySide6.QtPrintSupport import QPrintDialog, QPrinter, QPageSetupDialog
@@ -7,10 +7,21 @@ import qt_material as FormStyle
 from ui.form import Ui_MainWindow
 from ui.settings import Ui_Dialog as Ui_SettingsDialog
 from ui.about import Ui_Dialog as Ui_AboutDialog
-import configparser
 import logging
 import os
+import platform
+import traceback
+import yaml
 
+language = {
+    "en-us": "English (US)",
+    "zh-cn": "简体中文 (中国大陆)",
+}
+
+encodings = {
+    "utf-8": "UTF-8",
+    "gbk": "GBK",
+}
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,18 +31,21 @@ logging.basicConfig(
 logger = logging.getLogger("MainWindow")
 
 try:
-    config = configparser.ConfigParser()
-    config.read("./settings.properties")
-    language = config.get("form", "language")
-    style = config.get("form", "style")
-except configparser.NoSectionError:
-    logger.error("settings.properties is invalid or it does not exist. (configparser.NoSectionError)")
-    language = ""
-    style = ""
-# Read settings from settings.properties
+    with open("settings.yaml", "r", encoding="utf-8") as file:
+        settings = yaml.safe_load(file)
+except FileNotFoundError:
+    logger.critical("settings.yaml not found! The program cannot continue to run!")
+    logger.critical("Full traceback:\n" + traceback.format_exc())
+    exit(1)
+except yaml.YAMLError:
+    logger.critical("settings.yaml is not a valid YAML file! The program cannot continue to run!")
+    logger.critical("Full traceback:\n" + traceback.format_exc())
+    exit(1)
 
-if style not in FormStyle.list_themes():
-    logger.warning("A non-existent theme has appeared.")
+# Read settings from settings.yaml
+
+if settings["appearance"]["theme"] not in FormStyle.list_themes():
+    logger.warning("An invalid style was detected.")
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -49,6 +63,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.actionPrint.triggered.connect(self.printFile)
         self.actionExit.triggered.connect(self.close)
         self.actionAbout.triggered.connect(self.aboutWindow)
+        self.actionView_README.triggered.connect(lambda: os.startfile("README.MD"))
+        self.actionOpen_with.triggered.connect(self.openWith)
+        self.actionOpen_in_System_File_Explorer.triggered.connect(self.openInFileExplorer)
         # Connect slots to the functions
         self.textEdit.textChanged.connect(self.textChanged)
         # Define variables
@@ -66,14 +83,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def aboutWindow(self):
         logger.info("About window called.")
         self.aboutDialog = AboutDialog()
-        self.aboutDialog.buttonBox.helpRequested.connect(self.help)
+        self.aboutDialog.buttonBox.helpRequested.connect(lambda: os.startfile("README.MD"))
         logger.info("About window opened.")
         self.aboutDialog.show()
 
     def openFile(self):
         logger.info("openFile function called.")
         self.filePath = QFileDialog.getOpenFileName(self, "Open a file", "", "All files(*.*)")[0]
-        if os.path.getsize(self.filePath) >= 1073741824:  # 1 GiB
+        if self.filePath == "": return
+        if os.path.getsize(self.filePath) >= 1 * 1024 * 1024:  # 1 MiB
             if not QMessageBox.question(
                     self, "Warning",
                     "This file looks very large and doesn't seem to be a plain text file. "
@@ -159,9 +177,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         if self.printDialog.exec() == QtWidgets.QDialog.DialogCode.Accepted:
             self.textEdit.print_(self.Printer)
 
-    def help(self):
-        logger.info("Help function called.")
-        # TODO: 完成帮助文档
+    def openInFileExplorer(self):
+        logger.info("openInFileExplorer function called.")
+        if platform.system() == "Windows":
+            os.system(f'explorer /select \"{self.filePath.replace("/", "\\")}\"')
+            logger.info(f"Ran command \"explorer /select \"{self.filePath}\"\"")
+        elif platform.system() == "Darwin":
+            os.system(f'open -R \"{self.filePath}\"')
+            logger.info(f"Ran command \"open -R \"{self.filePath}\"\"")
+        elif platform.system() == "Linux":
+            os.system(f'xdg-open \"{self.filePath}\"')
+            logger.info(f"Ran command \"xdg-open \"{self.filePath}\"\"")
+        else:
+            logger.warning("Could not find system's file explorer.")
+            QMessageBox.warning(self, "Warning", f"Could not find system's file explorer.\nThe file path is"
+                                                 f"\n{self.filePath}")
+
+    def openWith(self):
+        program = QInputDialog.getText(self, "Open with",
+                                       "Enter the path of the program you want to open with.")[0]
+        os.system(f'{program} "{self.filePath}"')
 
 
 class SettingsDialog(QtWidgets.QDialog, Ui_SettingsDialog):
@@ -170,6 +205,66 @@ class SettingsDialog(QtWidgets.QDialog, Ui_SettingsDialog):
         self.setupUi(self)
         self.setWindowIcon(QIcon("./ui/favicon.ico"))
         self.setFixedSize(self.size())
+        # Connect buttons to the functions
+        self.buttonBox.accepted.connect(self.saveSettings)
+        self.rejected.connect(self.reject)
+        self.pageSetupButton.clicked.connect(MainWindow.pageSetup)
+        # Connect events to the functions
+        self.timestampPresetsChoose.currentIndexChanged.connect(self.timestampPresetChanged)
+
+    def saveSettings(self):
+        logger.info("saveSettings function called.")
+        global settings
+        currentLanguage = ""
+        currentEncoding = ""
+        for i in list(language.values()):
+            if language[i] == self.languageChoose.currentText():
+                currentLanguage = i
+        for j in list(encodings.values()):
+            if encodings[j] == self.encodingChoose.currentText():
+                currentEncoding = j
+        settings = {
+            "region": {
+                "language": currentLanguage,
+                "encoding": currentEncoding,
+                "timestamp": self.customTimestampSet.currentText(),
+            },
+            "editor": {
+                "autoLineWrapping": self.autoLineWrappingSetting.isChecked(),
+            },
+            "appearance": {
+                "theme": self.themeChooseCombo.currentText().replace(" ", "_").lower(),
+                "font": self.fontSetting.currentText(),
+                "fontSize": int(self.fontSizeSetting.currentText()),
+            },
+        }
+        return self.accept
+
+    def timestampPresetChanged(self):
+        logger.info("timestampPresetChanged function called.")
+        if self.timestampPresetsChoose.currentIndex() == 4:
+            self.customTimestampSet.setEnabled(True)
+        else:
+            self.customTimestampSet.setEnabled(False)
+        match self.customTimestampSet.currentIndex():
+            case 0:
+                timestampFormat = "MM/DD/YY hh:mm:ss"
+                self.customTimestampSet.setText(timestampFormat)
+                self.customTimestampSet.setEnabled(False)
+            case 1:
+                timestampFormat = "DD/MM/YY hh:mm:ss"
+                self.customTimestampSet.setText(timestampFormat)
+                self.customTimestampSet.setEnabled(False)
+            case 2:
+                timestampFormat = "YYYY-MM-DD hh:mm:ss"
+                self.customTimestampSet.setText(timestampFormat)
+                self.customTimestampSet.setEnabled(False)
+            case 3:
+                timestampFormat = "YYYY-MM-DDThh:mm:ss"
+                self.customTimestampSet.setText(timestampFormat)
+                self.customTimestampSet.setEnabled(False)
+            case 4:
+                self.customTimestampSet.setEnabled(True)
 
 
 class AboutDialog(QtWidgets.QDialog, Ui_AboutDialog):
@@ -185,15 +280,17 @@ def entryPoint(*args):
     if app == None:
         app = QApplication(*args)
     translator = QTranslator()
-    if translator.load(language, "./ui/i18n"):
+    if translator.load(settings["region"]["language"], "./ui/i18n"):
         app.installTranslator(translator)
     window = MainWindow()
     window.setWindowIcon(QIcon("./ui/favicon.ico"))
     window.setWindowTitle("Whale - New File")
-    FormStyle.apply_stylesheet(app, style)
+    FormStyle.apply_stylesheet(app, settings["appearance"]["theme"])
     logger.info("Application started.")
     window.show()
-    exit(app.exec())
+    exitCode = app.exec()
+    logger.info(f"Application closed with exit code {exitCode}.")
+    exit(exitCode)
 
 
 if __name__ == "__main__":
--- ui/settings.py
+++ ui/settings.py
@@ -3,7 +3,7 @@
 ################################################################################
 # Form generated from reading UI file 'settings.ui'
 #
-# Created by: Qt User Interface Compiler version 6.8.0
+# Created by: Qt User Interface Compiler version 6.8.2
 #
 # WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -25,9 +25,9 @@ class Ui_Dialog(object):
         self.buttonBox.setGeometry(QRect(10, 240, 471, 32))
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok)
-        self.tabWidget = QTabWidget(Dialog)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(0, 0, 481, 241))
+        self.settingsTabs = QTabWidget(Dialog)
+        self.settingsTabs.setObjectName(u"settingsTabs")
+        self.settingsTabs.setGeometry(QRect(0, 0, 481, 241))
         self.generalSettings = QWidget()
         self.generalSettings.setObjectName(u"generalSettings")
         self.languageChooseLabel = QLabel(self.generalSettings)
@@ -50,10 +50,9 @@ class Ui_Dialog(object):
         self.encodingChoose = QComboBox(self.generalSettings)
         self.encodingChoose.addItem("")
         self.encodingChoose.addItem("")
-        self.encodingChoose.addItem("")
         self.encodingChoose.setObjectName(u"encodingChoose")
         self.encodingChoose.setGeometry(QRect(140, 110, 321, 22))
-        self.tabWidget.addTab(self.generalSettings, "")
+        self.settingsTabs.addTab(self.generalSettings, "")
         self.editorSettings = QWidget()
         self.editorSettings.setObjectName(u"editorSettings")
         self.fontSettingLabel = QLabel(self.editorSettings)
@@ -83,22 +82,63 @@ class Ui_Dialog(object):
         self.customTimestampSet.setObjectName(u"customTimestampSet")
         self.customTimestampSet.setEnabled(False)
         self.customTimestampSet.setGeometry(QRect(10, 79, 441, 31))
-        self.tabWidget.addTab(self.editorSettings, "")
+        self.settingsTabs.addTab(self.editorSettings, "")
+        self.appearanceSettings = QWidget()
+        self.appearanceSettings.setObjectName(u"appearanceSettings")
+        self.themeChooseLabel = QLabel(self.appearanceSettings)
+        self.themeChooseLabel.setObjectName(u"themeChooseLabel")
+        self.themeChooseLabel.setGeometry(QRect(10, 10, 54, 16))
+        self.themeChooseLabel.setAlignment(
+            Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTrailing | Qt.AlignmentFlag.AlignVCenter)
+        self.themeChooseCombo = QComboBox(self.appearanceSettings)
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.addItem("")
+        self.themeChooseCombo.setObjectName(u"themeChooseCombo")
+        self.themeChooseCombo.setGeometry(QRect(70, 10, 371, 22))
+        self.settingsTabs.addTab(self.appearanceSettings, "")
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
         self.buttonBox.rejected.connect(Dialog.reject)
 
-        self.tabWidget.setCurrentIndex(0)
+        self.settingsTabs.setCurrentIndex(0)
 
         QMetaObject.connectSlotsByName(Dialog)
+
     # setupUi
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Settings", None))
         self.languageChooseLabel.setText(QCoreApplication.translate("Dialog", u"Language Profile:", None))
         self.languageChoose.setItemText(0, QCoreApplication.translate("Dialog", u"English (US)", None))
-        self.languageChoose.setItemText(1, QCoreApplication.translate("Dialog", u"Simp. Chinese (China Mainland)",
+        self.languageChoose.setItemText(1, QCoreApplication.translate("Dialog",
+                                                                      u"\u7b80\u4f53\u4e2d\u6587 ("
+                                                                      u"\u4e2d\u56fd\u5927\u9646)",
                                                                       None))
 
         self.autoLineWrappingSetting.setText(QCoreApplication.translate("Dialog", u"Automatic line wrapping", None))
@@ -106,34 +146,66 @@ class Ui_Dialog(object):
         self.encodingChooseLabel.setText(QCoreApplication.translate("Dialog", u"Default File Encoding:", None))
         self.encodingChoose.setItemText(0, QCoreApplication.translate("Dialog", u"UTF-8", None))
         self.encodingChoose.setItemText(1, QCoreApplication.translate("Dialog", u"GBK", None))
-        self.encodingChoose.setItemText(2, QCoreApplication.translate("Dialog", u"ANSI", None))
 
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.generalSettings), QCoreApplication.translate("Dialog",
-                                                                                                           u"General",
-                                                                                                           None))
+        self.settingsTabs.setTabText(self.settingsTabs.indexOf(self.generalSettings),
+                                     QCoreApplication.translate("Dialog", u"General", None))
         self.fontSettingLabel.setText(QCoreApplication.translate("Dialog", u"Editor Font:", None))
         self.fontPreview.setText(QCoreApplication.translate("Dialog", u"Type here to preview your settings...", None))
         self.timestampSettings.setTitle(QCoreApplication.translate("Dialog", u"Timestamp Settings", None))
         self.timestampPresetsChoose.setItemText(0, QCoreApplication.translate("Dialog",
                                                                               u"Western Countries (MM-DD-YY hh:mm:ss, "
-                                                                              u"example: 02-16-25 13:06:35)", None))
+                                                                              u"example: 02-16-25 13:06:35)",
+                                                                              None))
         self.timestampPresetsChoose.setItemText(1, QCoreApplication.translate("Dialog",
-                                                                              u"UK Style (DD-MM-YY hh:mm:ss, "
-                                                                              u"example: 16-02-15 13:06:35)", None))
+                                                                              u"UK Style (DD-MM-YY hh:mm:ss, example: "
+                                                                              u"16-02-15 13:06:35)",
+                                                                              None))
         self.timestampPresetsChoose.setItemText(2, QCoreApplication.translate("Dialog",
-                                                                              u"China Mainland Style "
-                                                                              u"(YYYY-MM-DD hh:mm:ss, "
-                                                                              u"example: 2025-02-16 13:06:35)", None))
+                                                                              u"China Mainland Style (YYYY-MM-DD "
+                                                                              u"hh:mm:ss, example: 2025-02-16 "
+                                                                              u"13:06:35)",
+                                                                              None))
         self.timestampPresetsChoose.setItemText(3, QCoreApplication.translate("Dialog",
                                                                               u"ISO-8601 (YYYY-MM-DDThh:mm:ss, "
-                                                                              u"example: 2025-02-16T13:06:35)", None))
+                                                                              u"example: 2025-02-16T13:06:35)",
+                                                                              None))
         self.timestampPresetsChoose.setItemText(4, QCoreApplication.translate("Dialog", u"No Presets", None))
 
-# if QT_CONFIG(tooltip)
+        # if QT_CONFIG(tooltip)
         self.timestampPresetsChoose.setToolTip(QCoreApplication.translate("Dialog", u"Presets", None))
-# endif // QT_CONFIG(tooltip)
+        # endif // QT_CONFIG(tooltip)
         self.customTimestampSet.setText(QCoreApplication.translate("Dialog", u"MM-DD-YY hh:mm:ss", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.editorSettings), QCoreApplication.translate("Dialog",
-                                                                                                          u"Editor",
-                                                                                                          None))
+        self.settingsTabs.setTabText(self.settingsTabs.indexOf(self.editorSettings),
+                                     QCoreApplication.translate("Dialog", u"Editor", None))
+        self.themeChooseLabel.setText(QCoreApplication.translate("Dialog", u"Theme", None))
+        self.themeChooseCombo.setItemText(0, QCoreApplication.translate("Dialog", u"Dark Amber", None))
+        self.themeChooseCombo.setItemText(1, QCoreApplication.translate("Dialog", u"Dark Blue", None))
+        self.themeChooseCombo.setItemText(2, QCoreApplication.translate("Dialog", u"Dark Cyan", None))
+        self.themeChooseCombo.setItemText(3, QCoreApplication.translate("Dialog", u"Dark Lightgreen", None))
+        self.themeChooseCombo.setItemText(4, QCoreApplication.translate("Dialog", u"Dark Medical", None))
+        self.themeChooseCombo.setItemText(5, QCoreApplication.translate("Dialog", u"Dark Pink", None))
+        self.themeChooseCombo.setItemText(6, QCoreApplication.translate("Dialog", u"Dark Purple", None))
+        self.themeChooseCombo.setItemText(7, QCoreApplication.translate("Dialog", u"Dark Red", None))
+        self.themeChooseCombo.setItemText(8, QCoreApplication.translate("Dialog", u"Dark Teal", None))
+        self.themeChooseCombo.setItemText(9, QCoreApplication.translate("Dialog", u"Dark Yellow", None))
+        self.themeChooseCombo.setItemText(10, QCoreApplication.translate("Dialog", u"Light Amber", None))
+        self.themeChooseCombo.setItemText(11, QCoreApplication.translate("Dialog", u"Light Blue", None))
+        self.themeChooseCombo.setItemText(12, QCoreApplication.translate("Dialog", u"Light Blue 500", None))
+        self.themeChooseCombo.setItemText(13, QCoreApplication.translate("Dialog", u"Light Cyan", None))
+        self.themeChooseCombo.setItemText(14, QCoreApplication.translate("Dialog", u"Light Cyan 500", None))
+        self.themeChooseCombo.setItemText(15, QCoreApplication.translate("Dialog", u"Light Lightgreen", None))
+        self.themeChooseCombo.setItemText(16, QCoreApplication.translate("Dialog", u"Light Lightgreen 500", None))
+        self.themeChooseCombo.setItemText(17, QCoreApplication.translate("Dialog", u"Light orange", None))
+        self.themeChooseCombo.setItemText(18, QCoreApplication.translate("Dialog", u"Light Pink", None))
+        self.themeChooseCombo.setItemText(19, QCoreApplication.translate("Dialog", u"Light Pink 500", None))
+        self.themeChooseCombo.setItemText(20, QCoreApplication.translate("Dialog", u"Light Purple", None))
+        self.themeChooseCombo.setItemText(21, QCoreApplication.translate("Dialog", u"Light Purple 500", None))
+        self.themeChooseCombo.setItemText(22, QCoreApplication.translate("Dialog", u"Light Red", None))
+        self.themeChooseCombo.setItemText(23, QCoreApplication.translate("Dialog", u"Light Red 500", None))
+        self.themeChooseCombo.setItemText(24, QCoreApplication.translate("Dialog", u"Light Teal", None))
+        self.themeChooseCombo.setItemText(25, QCoreApplication.translate("Dialog", u"Light Teal 500", None))
+        self.themeChooseCombo.setItemText(26, QCoreApplication.translate("Dialog", u"Light Yellow", None))
+
+        self.settingsTabs.setTabText(self.settingsTabs.indexOf(self.appearanceSettings),
+                                     QCoreApplication.translate("Dialog", u"Appearance", None))
     # retranslateUi
